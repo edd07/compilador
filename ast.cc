93cac09b44f58f96de9798f309094c2fabda02f1
Juan Jose de Miguel <juanjosedemiguel@gmail.com>
2013-05-19 12:18:13 -0500

    diferentes razones de error

--- ast.cc
+++ ast.cc
@@ -32,6 +32,33 @@ Identifier::Identifier(yyltype loc, const char *n) : Node(loc) {
 void Identifier::Check(){
 	//aqui es donde pasa toda la magia
 	Node* ptr = this->parent;
+	reasonT reason;
+	
+	VarDecl* v = dynamic_cast<VarDecl*>(ptr);
+    if(v != 0) 
+        reason = LookingForVariable;
+	
+	Type* t = dynamic_cast<Type*>(ptr);
+    NamedType* n = dynamic_cast<NamedType*>(ptr);
+    if( t != 0 || n != 0) {
+        if (dynamic_cast<InterfaceDecl*>(ptr) != 0 )
+            reason = LookingForInterface;
+        else
+            reason = LookingForType;
+    }
+    
+	ClassDecl* c = dynamic_cast<ClassDecl*>(ptr);
+    if ( c != 0)
+       reason = LookingForClass;
+	
+	FnDecl* f = dynamic_cast<FnDecl*>(ptr);
+    if(f != 0)
+       reason = LookingForFunction;
+    
+    ArrayType* a = dynamic_cast<ArrayType*>(ptr);
+    if( a != 0 )
+       reason = LookingForType;
+	
 	bool flag = true;
 	while(flag && ptr->table->Lookup(name)==NULL ){
 		ptr = ptr->parent;
@@ -39,8 +66,9 @@ void Identifier::Check(){
 		if(prg!=NULL) flag=false;
 	}
 	
-	if(!flag){	
-		ReportError::IdentifierNotDeclared(this, LookingForVariable);
+	if(!flag){
+	
+		ReportError::IdentifierNotDeclared(this, reason);
 	}
 }
 
